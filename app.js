7f01bf33d952d876a10f09a47aeb89897e3ef3f7
pan <panarcadia@proton.me>
2023-12-27 11:11:11 -0700

    save

--- app.js
+++ app.js
@@ -22,13 +22,15 @@ function addTodo(todo) {
     let li = document.createElement('li')
 
     li.innerHTML = `
-        <span class="todo-item">${todo}</span>
-        <button name="checkButton">
-            <i class="fas fa-check-square"></i>
-        </button>
-        <button name="deleteButton">
-            <i class="fas fa-trash"></i>
-        </button>
+        <div class="todoItem">
+            <span>${todo}</span>
+            <button name="checkButton">
+                <i class="fas fa-check-square"></i>
+            </button>
+            <button name="deleteButton">
+                <i class="fas fa-trash"></i>
+            </button>
+        </div>
     `
 
     ul.appendChild(li)
